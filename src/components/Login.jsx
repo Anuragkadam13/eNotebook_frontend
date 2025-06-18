fdd1620a67335d3efaabe488c24dee295e988ae0
Anurag Kadam <kadamanurag819@gmail.com>
2025-06-18 16:38:55 +0530

    login page problem fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
