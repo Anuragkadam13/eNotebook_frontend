e24a1eec9ab7ee1116aee5c70b77fe2b8d7445e1
Anurag Kadam <kadamanurag819@gmail.com>
2025-07-29 13:48:02 +0530

    some bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="fixed w-full z-100  ">
+    <div className="fixed w-full z-50">
       <nav className="px-5 py-2 flex items-center justify-between font-medium shadow bg-white dark:bg-[#0A0A0A] dark:border-b">
         <div className="flex items-center gap-1 ">
           <img src={logo} className="h-12 md:h-14" alt="" />
@@ -40,12 +40,12 @@ const Navbar = () => {
                   <X className="w-fit h-7" />
                 )}
               </PopoverTrigger>
-              <PopoverContent className="z-100">
+              <PopoverContent className="z-60">
                 <div className="w-full ">
-                  <About className="z-100" />
-                  <ModeToggle className="z-100" />
+                  <About className="z-70" />
+                  <ModeToggle className="z-70" />
                   <Button
-                    className="hover:cursor-pointer w-full justify-start z-100"
+                    className="hover:cursor-pointer w-full justify-start "
                     variant="ghost"
                     onClick={handleLogout}
                   >
